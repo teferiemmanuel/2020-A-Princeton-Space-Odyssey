d586cf9c48038d8dd81c1fb56a263d55f8dec375
justin.tran.jt <justin.tran.jt@gmail.com>
2020-05-08 16:20:51 -0700

    Added audio for fuel pickup

--- src/components/objects/Player/Player.js
+++ src/components/objects/Player/Player.js
@@ -5,7 +5,6 @@ import AsteroidCollision from '../../audio/asteroid_collision.mp3';
 import FuelPickup from '../../audio/fuel_pickup.mp3';
 import PowerupPickup from '../../audio/powerup_pickup.mp3';
 
-
 class Player extends Group {
     constructor(parent, positionVec, world) {
         // Call parent Group() constructor
@@ -33,64 +32,76 @@ class Player extends Group {
         this.body = body;
         this.body.gameScene = parent;
 
-        this.body.addEventListener("collide",function(e){
-          console.log(this);
+        this.body.addEventListener('collide', function (e) {
+            console.log(this);
 
-          if (e.body.asteroid !== undefined) {
-            document.getElementById('collisionMessage').innerHTML =
-                'OOF! You lost some fuel';
+            if (e.body.asteroid !== undefined) {
+                document.getElementById('collisionMessage').innerHTML =
+                    'OOF! You lost some fuel';
 
-            this.gameScene.gameTimeRem -= 5;
+                this.gameScene.gameTimeRem -= 5;
 
-            // create an audio source
-            const soundEffect = new Audio(this.gameScene.listener);
-            // load a sound and set it as the Audio object's buffer
-            const audioLoader = new AudioLoader();
-            audioLoader.load(AsteroidCollision, function (buffer) {
-                soundEffect.setBuffer(buffer);
-                soundEffect.setLoop(false);
-                soundEffect.setVolume(0.15);
-                soundEffect.play();
-            });
+                // create an audio source
+                const soundEffect = new Audio(this.gameScene.listener);
+                // load a sound and set it as the Audio object's buffer
+                const audioLoader = new AudioLoader();
+                audioLoader.load(AsteroidCollision, function (buffer) {
+                    soundEffect.setBuffer(buffer);
+                    soundEffect.setLoop(false);
+                    soundEffect.setVolume(0.15);
+                    soundEffect.play();
+                });
 
-            // Handle time elapsed
-            if (
-                this.gameScene.gameTimeRem + 5 >
-                this.gameScene.MAX_FUEL_SECONDS
-            )
-                this.gameScene.gameTimeRem = this.gameScene.MAX_FUEL_SECONDS;
-            else this.gameScene.gameTimeRem += 3;
-          }
-          else if (e.body.fuel !== undefined) {
-            document.getElementById('collisionMessage').innerHTML =
-                'Fuel recharged!';
-            this.gameScene.numCollectedFuels++;
-            this.gameScene.numSpawnedFuels--;
+                // Handle time elapsed
+                if (
+                    this.gameScene.gameTimeRem + 5 >
+                    this.gameScene.MAX_FUEL_SECONDS
+                )
+                    this.gameScene.gameTimeRem = this.gameScene.MAX_FUEL_SECONDS;
+                else this.gameScene.gameTimeRem += 3;
+            } else if (e.body.fuel !== undefined) {
+                document.getElementById('collisionMessage').innerHTML =
+                    'Fuel recharged!';
+                this.gameScene.numCollectedFuels++;
+                this.gameScene.numSpawnedFuels--;
 
-            // Handle time elapsed
-            if (this.gameScene.gameTimeRem + 5 > this.gameScene.MAX_FUEL_SECONDS)
-                tihs.gameScene.gameTimeRem = this.gameScene.MAX_FUEL_SECONDS;
-            else this.gameScene.gameTimeRem += 3;
-            this.gameScene.handleCollectedFuel(e.body.fuel);
-          }
-          else if (e.body.powerup !== undefined) {
-            document.getElementById('collisionMessage').innerHTML =
-                'Temporary Invincibility';
-            // TODO: Somebody implement invincibility here?
+                // create an audio source
+                const soundEffect = new Audio(this.gameScene.listener);
+                // load a sound and set it as the Audio object's buffer
+                const audioLoader = new AudioLoader();
+                audioLoader.load(FuelPickup, function (buffer) {
+                    soundEffect.setBuffer(buffer);
+                    soundEffect.setLoop(false);
+                    soundEffect.setVolume(0.15);
+                    soundEffect.play();
+                });
 
-            // create an audio source
-            const soundEffect = new Audio(this.gameScene.listener);
-            // load a sound and set it as the Audio object's buffer
-            const audioLoader = new AudioLoader();
-            audioLoader.load(PowerupPickup, function (buffer) {
-                soundEffect.setBuffer(buffer);
-                soundEffect.setLoop(false);
-                soundEffect.setVolume(0.15);
-                soundEffect.play();
-            });
+                // Handle time elapsed
+                if (
+                    this.gameScene.gameTimeRem + 5 >
+                    this.gameScene.MAX_FUEL_SECONDS
+                )
+                    tihs.gameScene.gameTimeRem = this.gameScene.MAX_FUEL_SECONDS;
+                else this.gameScene.gameTimeRem += 3;
+                this.gameScene.handleCollectedFuel(e.body.fuel);
+            } else if (e.body.powerup !== undefined) {
+                document.getElementById('collisionMessage').innerHTML =
+                    'Temporary Invincibility';
+                // TODO: Somebody implement invincibility here?
 
-            this.gameScene.handlePowerupCollision(e.body.powerup);
-          }
+                // create an audio source
+                const soundEffect = new Audio(this.gameScene.listener);
+                // load a sound and set it as the Audio object's buffer
+                const audioLoader = new AudioLoader();
+                audioLoader.load(PowerupPickup, function (buffer) {
+                    soundEffect.setBuffer(buffer);
+                    soundEffect.setLoop(false);
+                    soundEffect.setVolume(0.15);
+                    soundEffect.play();
+                });
+
+                this.gameScene.handlePowerupCollision(e.body.powerup);
+            }
         });
 
         // debugging mesh just in case we need to visualize boudingSphere
